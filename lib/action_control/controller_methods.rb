4d9e3f871770d27fea96b8b9f56c6f44d22f13cb
Tobias Feistmantl <tobias@feistmantl.io>
2016-08-09 11:09:52 +0200

    Add helpers to identify index and show actions

--- lib/action_control/controller_methods.rb
+++ lib/action_control/controller_methods.rb
@@ -7,28 +7,42 @@ module ActionControl
 	#    True if the called action
 	#    is a only-read action.
 	def read_action?
-		action_name == "index" ||
-		action_name == "show"
+		action_name == 'index' ||
+		action_name == 'show'
 	end
 
 	# @return [Boolean]
 	#    True if the called action
 	#    is a write action.
 	def write_action?
-		action_name == "new" ||
-		action_name == "create" ||
-		action_name == "edit" ||
-		action_name == "update" ||
-		action_name == "destroy"
+		action_name == 'new' ||
+		action_name == 'create' ||
+		action_name == 'edit' ||
+		action_name == 'update' ||
+		action_name == 'destroy'
 	end
 
 	# @return [Boolean]
 	#    True if the called action
 	#    is a change action.
 	def change_action?
-		action_name == "edit" ||
-		action_name == "update" ||
-		action_name == "destroy"
+		action_name == 'edit' ||
+		action_name == 'update' ||
+		action_name == 'destroy'
+	end
+
+	# @return [Boolean]
+	#    True if the called action
+	#    is the index action.
+	def index_action?
+		action_name == 'index'
+	end
+
+	# @return [Boolean]
+	#    True if the called action
+	#    is the show action.
+	def show_action?
+		action_name == 'show'
 	end
 
 	# @note
@@ -43,8 +57,8 @@ module ActionControl
 	#    True if the called action
 	#    is a create action.
 	def create_action?
-		action_name == "new" ||
-		action_name == "create"
+		action_name == 'new' ||
+		action_name == 'create'
 	end
 
 	# @note
@@ -55,14 +69,14 @@ module ActionControl
 	#    True if the called action
 	#    is a update action.
 	def update_action?
-		action_name == "edit" ||
-		action_name == "update"
+		action_name == 'edit' ||
+		action_name == 'update'
 	end
 
 	# @return [Boolean]
 	#    True if it's a destroy action.
 	def destroy_action?
-		action_name == "destroy"
+		action_name == 'destroy'
 	end
 
 	alias delete_action? destroy_action?
